5a277f4de6a4bd7f0029f675f8ab66c29c3df5fd
PhuongPham1203 <Phamphuong120398@gmail.com>
2022-03-18 09:48:43 +0700

    Update pol.js

--- WebPurchaseOrder6/WebPurchaseOrder6/Assets/js/pol.js
+++ WebPurchaseOrder6/WebPurchaseOrder6/Assets/js/pol.js
@@ -16,6 +16,21 @@ function viewModelPOD() {
 
 }
 
+function viewModelPOL() {
+    var self = this;
+    id: null;
+    part_number: null;
+    part_descripttion: null;
+    manufacturer: null;
+
+    qty_ordered: null;
+    order_date: null;
+    m2_buy_price: null;
+    memo: null;
+
+    total_price_in_line: null
+}
+
 $(document).ready(function () {
     $.ajax({
 
@@ -41,33 +56,61 @@ $(document).ready(function () {
             vm.country = response.purchase_order.country;
             vm.post_code = response.purchase_order.post_code;
 
-
             for (let i = 0; i < response.part.length; i++) {
-                
+
+                var item_mode = new viewModelPOL();
+                item_mode.id = ko.observable(response.part[i].id)
+                item_mode.part_number = ko.observable(response.part[i].part_number);
+                item_mode.part_descripttion = ko.observable(response.part[i].part_descripttion);
+                item_mode.manufacturer = ko.observable(response.part[i].manufacturer);
+
+                item_mode.qty_ordered = ko.observable(response.purchase_order_line[i].qty_ordered);
+                item_mode.order_date = ko.observable(timestamp2Date(response.purchase_order_line[i].order_date));
+                item_mode.m2_buy_price = ko.observable(response.purchase_order_line[i].m2_buy_price);
+                item_mode.memo = ko.observable(response.purchase_order_line[i].memo);
+
+                item_mode.total_price_in_line = ko.computed(function () {
+
+                    return item_mode.qty_ordered() * item_mode.m2_buy_price();
+                })
+                //ko.applyBindings(item_mode);
+                vm.purchase_order_line.push(item_mode);
+
+            }
+
+            /*
+            for (let i = 0; i < response.part.length; i++) {
+
                 var item_data = {
+                    
                     id: response.part[i].id,
                     part_number: response.part[i].part_number,
                     part_descripttion: response.part[i].part_descripttion,
                     manufacturer: response.part[i].manufacturer,
 
-                    qty_ordered: response.purchase_order_line[i].qty_ordered,
+                    //qty_ordered: response.purchase_order_line[i].qty_ordered,
                     order_date: timestamp2Date(response.purchase_order_line[i].order_date),
-                    m2_buy_price: response.purchase_order_line[i].m2_buy_price,
+                    //m2_buy_price: response.purchase_order_line[i].m2_buy_price,
                     memo: response.purchase_order_line[i].memo,
 
-                    total_price_in_line: ko.computed(function () {
-                        
-                        return response.purchase_order_line[i].qty_ordered * response.purchase_order_line[i].m2_buy_price;
-                    })
                 };
+
+                item_data.qty_ordered = ko.observable(response.purchase_order_line[i].qty_ordered);
+                item_data.m2_buy_price = ko.observable(response.purchase_order_line[i].m2_buy_price);
+
+                item_data.total_price_in_line = ko.computed(function () {
+
+                    return item_data.qty_ordered() * item_data.m2_buy_price();
+                })
+
                 vm.purchase_order_line.push(item_data);
 
             }
+            */
 
 
             ko.applyBindings(vm);
-            console.log(vm);
-
+            //console.log(vm);
         },
         error: function (response) {
             console.log("error GetData");
