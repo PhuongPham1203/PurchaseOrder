d999d21c078f4ed75aca06fa3edff9314152c919
PhuongPham1203 <Phamphuong120398@gmail.com>
2022-03-25 17:36:01 +0700

    update change, delete, add POLine in PurchaseOrderDetail

--- Angular/App_Angular/src/app/purchase-order-detail/purchase-order-detail.component.ts
+++ Angular/App_Angular/src/app/purchase-order-detail/purchase-order-detail.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { DatetimeService } from '../Services/datetime.service';
 import { DomainAPIService } from '../Services/domain-api.service';
 import { ServerHttpService } from '../Services/server-http.service';
+import { cloneDeep } from 'lodash';
 
 @Component({
 	selector: 'app-purchase-order-detail',
@@ -19,47 +20,181 @@ export class PurchaseOrderDetailComponent implements OnInit {
 	) { }
 
 	public dataPODetail = null;
+	public listPartNumberSelected = [];
+	//public listPartsNotAvalibleSelect = [];
 
 	ngOnInit(): void {
 
 		var index = this.router.url.split("/").pop()
 
-		this.updatePurchaserOrderDetail(index);
+		this.UpdatePurchaserOrderDetail(index);
 
 
 	}
 
 	// display Purchase Order Detail
-	public updatePurchaserOrderDetail(index) {
+	public UpdatePurchaserOrderDetail(index) {
 		var url = this.domainAPI.getUrlPO() + "/PurchaseOrderDetail/GetPurchaseOrderDetail/" + index;
 
 		this.serverHttp.getAPI(url).subscribe((data) => {
 			this.dataPODetail = data;
+			//console.log(this.dataPODetail);
 
 			try {
 				this.dataPODetail.orderDate = this.datetimeFormat.string2DataInputForm(this.dataPODetail.orderDate);
+
+				//this.listPartsAvailableSelect = cloneDeep(this.dataPODetail.listAvailablePart);
+
 				for (let i = 0; i < this.dataPODetail.purchaseOrderLines.length; i++) {
 					var dateInput = this.datetimeFormat.string2DataInputForm(this.dataPODetail.purchaseOrderLines[i].orderDate);
 					this.dataPODetail.purchaseOrderLines[i].orderDate = dateInput;
+
+					// remove Part number already in POL
+					this.listPartNumberSelected.push(this.dataPODetail.purchaseOrderLines[i].partNumber);
+
 				}
+
 			} catch (e) {
 
 			}
+
 		})
 	}
 
-	public totalPricePO(){
-		var sum=0;
-		for(let item of this.dataPODetail.purchaseOrderLines){
-			sum+=item.qtyOrdered*item.m2BuyPrice
+	// sum price of all PO Line in PO
+	public TotalPricePO() {
+		var sum = 0;
+		for (let item of this.dataPODetail.purchaseOrderLines) {
+			sum += item.qtyOrdered * item.m2BuyPrice
 		}
 
 		return sum;
 	}
 
-	public deletePurchaseOrderLine(indexPOL){
+	
 
-		console.log("deletePurchaseOrderLine: "+indexPOL);
+	public RemoveMultipleItemFromList(original, remove) {
+		return original.filter(value => !remove.includes(value));
 	}
+
+	public GetItemInListBy_PartNumber(listParts, partNumber) {
+		var part = null;
+		listParts.forEach((value, index) => {
+			if (value.partNumber === partNumber) {
+				part = cloneDeep(value);
+			}
+		});
+
+		return part;
+	}
+
+	public RemoveItemInListBy_PartNumber(listParts, partNumber) {
+		listParts.forEach((value, index) => {
+			if (value.partNumber == partNumber) {
+				listParts.splice(index, 1);
+			}
+		});
+		return listParts;
+	}
+
+	public GetListPartCanSelectedInPOL(except) {
+
+		var listPart = cloneDeep(this.dataPODetail.listAvailablePart);
+		var listPS = cloneDeep(this.listPartNumberSelected);
+		listPS.forEach((value, index) => {
+			if (value == except) listPS.splice(index, 1);
+		});
+		for (let pn of listPS) {
+			listPart = this.RemoveItemInListBy_PartNumber(listPart, pn);
+		}
+
+		return listPart;
+	}
+
+	public OnChangePartNumberOnPOLine(event, indexPOL_In_purchaseOrderLines) {
+
+		var partNumberOnChange = event.target.value;
+		var partOnChange = this.GetItemInListBy_PartNumber(this.dataPODetail.listAvailablePart, partNumberOnChange);
+
+		var partNumberCurrent = this.dataPODetail.purchaseOrderLines[indexPOL_In_purchaseOrderLines].partNumber;
+		var partCurrent = this.GetItemInListBy_PartNumber(this.dataPODetail.listAvailablePart, partNumberCurrent);
+
+		// delete partNumber current in List Part Number Selected
+		this.listPartNumberSelected.forEach((value, index) => {
+			if (value == partNumberCurrent) {
+				this.listPartNumberSelected.splice(index, 1);
+			}
+		});
+
+		// add partNumber On Change to List Part Number Selected
+		this.listPartNumberSelected.push(partNumberOnChange);
+
+		// change part in PO Line in PO Detail
+		this.ChangePOLine_In_PODetail(indexPOL_In_purchaseOrderLines, partOnChange);
+
+	}
+
+	public ChangePOLine_In_PODetail(idPOLine, part) {
+		this.dataPODetail.purchaseOrderLines[idPOLine].idPart = part.id;
+		this.dataPODetail.purchaseOrderLines[idPOLine].partNumber = part.partNumber;
+		this.dataPODetail.purchaseOrderLines[idPOLine].partDescripttion = part.partDescripttion;
+		this.dataPODetail.purchaseOrderLines[idPOLine].manufacturer = part.manufacturer;
+
+		this.dataPODetail.purchaseOrderLines[idPOLine].qtyOrdered = 1;
+		this.dataPODetail.purchaseOrderLines[idPOLine].orderDate = this.dataPODetail.orderDate;
+		this.dataPODetail.purchaseOrderLines[idPOLine].m2BuyPrice = 0;
+		this.dataPODetail.purchaseOrderLines[idPOLine].memo = "";
+
+	}
+
+	// Add Purchase Order Line
+	public AddPurchaseOrderLine() {
+		if(this.listPartNumberSelected.length == this.dataPODetail.listAvailablePart.length){
+			alert("Cant Add More Purchase order Line!");
+			return;
+		}
+		var part = this.GetListPartCanSelectedInPOL(null)[0];
+		var pol = {
+			"id": null, 
+			"idPurchaseOrder": this.dataPODetail.orderNo, 
+			"orderDate": this.dataPODetail.orderDate, 
+			"qtyOrdered": 1, 
+			"backOrder": true, 
+			"m2BuyPrice": 0, 
+			"memo": "", 
+			"status": true, 
+			"idPart": part.id, 
+			"partNumber": part.partNumber, 
+			"partDescripttion": part.partDescripttion, 
+			"manufacturer": part.manufacturer
+		};
+
+		// add partNumber On Change to List Part Number Selected
+		this.listPartNumberSelected.push(part.partNumber);
+
+		this.dataPODetail.purchaseOrderLines.push(pol);
+	}
+
+	// delete column POL in PO 
+	public DeletePurchaseOrderLine(indexPOL) {
+
+		if(this.listPartNumberSelected.length<=1){
+			alert("Waring: The PO must have at least one PO line!");
+			return;
+		}
+
+		// delete partNumber current in List Part Number Selected
+		this.listPartNumberSelected.forEach((value, index) => {
+			if (value == this.dataPODetail.purchaseOrderLines[indexPOL].partNumber) {
+				this.listPartNumberSelected.splice(index, 1);
+			}
+		});
+
+		// delete PO Line
+		this.dataPODetail.purchaseOrderLines.splice(indexPOL,1);
+
+	}
+
+	
 
 }
